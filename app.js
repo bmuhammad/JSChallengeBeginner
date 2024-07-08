89bff5f83af45dd62eb54b1e363a68ee82b4f62e
Brian <briancmuhammad@yahoo.com>
2024-07-08 14:22:28 -0400

    third beginner challenge

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 //console.log(addition(-3,-6));
 //console.log(addition(7,3));
 
-function convertHoursToSeconds(hour){
+/*function convertHoursToSeconds(hour){
   
     let seconds = hour * 60 * 60;
 
@@ -16,4 +16,15 @@ function convertHoursToSeconds(hour){
 
 console.log(convertHoursToSeconds(2));
 console.log(convertHoursToSeconds(10));
-console.log(convertHoursToSeconds(24));
+console.log(convertHoursToSeconds(24)); */
+
+
+function calcPerimeter(length, width){
+
+   let perimeter = (2 * length) + (2 * width);
+    return perimeter;
+}
+
+console.log(calcPerimeter(6, 7));
+console.log(calcPerimeter(20, 10));
+console.log(calcPerimeter(2, 9));
