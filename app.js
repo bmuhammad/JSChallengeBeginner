9166df60e6bd9a701c9eca5ebe42befe846d12c4
Brian <briancmuhammad@yahoo.com>
2024-07-08 14:13:30 -0400

    second beginner challenge

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-function addition(num1, num2){
-    return num1 + num2;
+//function addition(num1, num2){
+   // return num1 + num2;
+//}
+
+//console.log(addition(3,2));
+//console.log(addition(-3,-6));
+//console.log(addition(7,3));
+
+function convertHoursToSeconds(hour){
+  
+    let seconds = hour * 60 * 60;
+
+    return seconds;
+
 }
 
-console.log(addition(3,2));
-console.log(addition(-3,-6));
-console.log(addition(7,3));
+console.log(convertHoursToSeconds(2));
+console.log(convertHoursToSeconds(10));
+console.log(convertHoursToSeconds(24));
