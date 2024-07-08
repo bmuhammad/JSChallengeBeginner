a8ceb447faae1d24dde328baf9f2f25903914a26
Brian <briancmuhammad@yahoo.com>
2024-07-08 14:38:19 -0400

    Beginner Q4 and Q5

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //function addition(num1, num2){
-   // return num1 + num2;
+// return num1 + num2;
 //}
 
 //console.log(addition(3,2));
@@ -18,8 +18,7 @@ console.log(convertHoursToSeconds(2));
 console.log(convertHoursToSeconds(10));
 console.log(convertHoursToSeconds(24)); */
 
-
-function calcPerimeter(length, width){
+/* function calcPerimeter(length, width){
 
    let perimeter = (2 * length) + (2 * width);
     return perimeter;
@@ -27,4 +26,26 @@ function calcPerimeter(length, width){
 
 console.log(calcPerimeter(6, 7));
 console.log(calcPerimeter(20, 10));
-console.log(calcPerimeter(2, 9));
+console.log(calcPerimeter(2, 9)); */
+
+/* function calcAreaOfTriangle(base, height) {
+  let triangleArea = (base / 2) * height;
+
+  return triangleArea;
+}
+
+console.log(calcAreaOfTriangle(3, 2));
+console.log(calcAreaOfTriangle(10, 10));
+console.log(calcAreaOfTriangle(20, 20)); */
+
+function extendString (fruit){
+
+    let extendedString = fruit + "Frontend";
+    return extendedString;
+}
+
+console.log(extendString("Apple"));
+console.log(extendString("Banana"));
+console.log(extendString("Orange"));
+
+
